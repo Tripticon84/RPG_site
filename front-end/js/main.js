f698248f688c9bca98507c4d897f50db66a893cf
Tripticon84 <yannjou@free.fr>
2024-05-19 17:24:05 +0200

    updated main.js :)

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -2,6 +2,7 @@ async function searchCampaigns() {
   const searchInput = document.getElementById("search");
   const s = searchInput.value;
 
+
   const sortInput = document.getElementById("sort");
   const sort = sortInput.value;
 
@@ -32,6 +33,42 @@ async function searchCampaigns() {
 
   const div = document.getElementById("result");
   div.innerHTML = html;
+
+  EasterEgg(s);
+
+}
+
+async function EasterEgg(searchInput) {
+  if (
+    searchInput === "Rick Astley" ||
+    searchInput === "rick astley" ||
+    searchInput === "Rick" ||
+    searchInput === "rick" ||
+    searchInput === "Astley" ||
+    searchInput === "astley" ||
+    searchInput === "Never" ||
+    searchInput === "never" ||
+    searchInput === "Gonna" ||
+    searchInput === "gonna" ||
+    searchInput === "Give" ||
+    searchInput === "give" ||
+    searchInput === "You" ||
+    searchInput === "you" ||
+    searchInput === "Up" ||
+    searchInput === "up" ||
+    searchInput === "Rickroll" ||
+    searchInput === "rickroll" ||
+    searchInput === "Rick Roll" ||
+    searchInput === "rick roll"
+  ) {
+
+    const div = document.getElementById("result");
+    div.innerHTML = '<iframe width="1920" height="500" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0&loop=1&mute=0&rel=0&fs=1&cc_load_policy=0&iv_load_policy=0&modestbranging=0&playsinline=1&playlist=dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+
+    exit();
+
+
+  }
 }
 
 async function displayChat() {
@@ -50,33 +87,32 @@ async function displayChat() {
   for (let i = 1; i < messages.length; i++) {
     const message = messages[i];
 
-    if (message !== null || message !== "" || message !== undefined){
+    if (message !== null || message !== "" || message !== undefined) {
     }
-      if (messages[0] !== message[1]) {
-        html += '<!-- Message -->'
-        html += `<div class="d-flex">`;
-        // html += `<img src="https://via.placeholder.com/50" alt="Avatar" class="rounded-circle m-2" width="50px" height="50px">`;
-        html += `<div class="bg-body-secondary p-1 me-5 rounded-4 m-2 p-2" style="width: fit-content;">`;
-        html += `<div class="fw-bold">${message[1]}</div>`;
-        html += `<div>${message[2]}</div>`;
-        html += `<div class="text-end text-body-secondary">${message[0]}</div>`;
-        html += `</div>`;
-        html += `</div>`;
-      } else {
-        html += '<!-- Message Soit -->'
-        html += `<div class="d-flex flex-row-reverse">`;
-        html += `<div class="bg-secondary rounded-4 p-2 ms-5 m-2 me-0" style="width: fit-content;">`;
-        html += `<div>${message[2]}</div>`;
-        html += `<div class="text-end text-body-secondary">${message[0]}</div>`;
-        html += `</div>`;
-        html += `</div>`;
-      }
-
+    if (messages[0] !== message[1]) {
+      html += "<!-- Message -->";
+      html += `<div class="d-flex">`;
+      // html += `<img src="https://via.placeholder.com/50" alt="Avatar" class="rounded-circle m-2" width="50px" height="50px">`;
+      html += `<div class="bg-body-secondary p-1 me-5 rounded-4 m-2 p-2" style="width: fit-content;">`;
+      html += `<div class="fw-bold">${message[1]}</div>`;
+      html += `<div>${message[2]}</div>`;
+      html += `<div class="text-end text-body-secondary">${message[0]}</div>`;
+      html += `</div>`;
+      html += `</div>`;
+    } else {
+      html += "<!-- Message Soit -->";
+      html += `<div class="d-flex flex-row-reverse">`;
+      html += `<div class="bg-secondary rounded-4 p-2 ms-5 m-2 me-0" style="width: fit-content;">`;
+      html += `<div>${message[2]}</div>`;
+      html += `<div class="text-end text-body-secondary">${message[0]}</div>`;
+      html += `</div>`;
+      html += `</div>`;
     }
-    const div = document.getElementById("chatResult");
-    div.innerHTML = html;
+  }
+  const div = document.getElementById("chatResult");
+  div.innerHTML = html;
 
-    div.scrollTop = div.scrollHeight;
+  div.scrollTop = div.scrollHeight;
 }
 
 async function sendChat() {
@@ -89,12 +125,12 @@ async function sendChat() {
   const res = await fetch("chat_send.php", {
     method: "POST",
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: `chatID=${chatID}&message=${message}`
+    body: `chatID=${chatID}&message=${message}`,
   });
-  
+
   messageInput.value = "";
 
-  await displayChat();  
-} 
+  await displayChat();
+}
